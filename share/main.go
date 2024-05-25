8c51e0fcc8f12e1bfead1952a1902e2dedd9e5f4
bingli <bingli@legend.tech>
2024-05-25 15:53:01 +0800

    debug share

--- share/main.go
+++ share/main.go
@@ -1,13 +1,20 @@
 package main
 
-//#include <file.h>
+/*
+#define BOOL int
+#define TRUE 1
+#define FALSE 0
+typedef struct{
+	char *errMsg;
+	char *data;
+	BOOL ok;
+} RSResult;
+*/
 import "C"
 
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/alecthomas/gometalinter/_linters/src/gopkg.in/yaml.v2"
-	"os"
 	"recovery-tool/cmd"
 	"recovery-tool/common"
 	"strconv"
@@ -81,24 +88,5 @@ func GoRecovery(zipPath, userMnemonic, eciesPrivKey, rsaPrivKey, vaultCount, cha
 }
 
 func main() {
-	//test
-	bytess, err := os.ReadFile("../input.yaml")
-	if err != nil {
-		panic(err)
-	}
-
-	var input cmd.RecoveryInput
-	if err := yaml.UnmarshalStrict(bytess, &input); err != nil {
-		panic(err)
-	}
-	input.ZipPath = "../test/134_archive.zip"
-	vaultCountStr := strconv.Itoa(input.VaultCount)
-	coinTypeStr := ""
-	for _, c := range input.CoinType {
-		coinTypeStr += strconv.Itoa(c) + ","
-	}
-	coinTypeStr = strings.TrimRight(coinTypeStr, ",")
-	res := GoRecovery(input.ZipPath, input.UserMnemonic, input.EciesPrivKey, input.RsaPrivKey, vaultCountStr, coinTypeStr)
-	fmt.Printf("res: %v \n", res)
 
 }
