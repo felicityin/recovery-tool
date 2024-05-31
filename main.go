4b0ed734761063fe5eb667522fae471bb9417a88
felicityin <yinjingping2022@gmail.com>
2024-05-31 17:33:48 +0800

    fix: eddsa

--- main.go
+++ main.go
@@ -9,16 +9,14 @@ import (
 )
 
 func main() {
-	var inputPath, outputPath string
-	flag.StringVar(&inputPath, "i", "./input.yaml", "the path of input parmas")
-	flag.StringVar(&outputPath, "o", "./output.yaml", "the path of result")
+	inputPath := flag.String("i", "./input.yaml", "the path of input parmas")
+	outputPath := flag.String("o", "./output.yaml", "the path of result")
+	flag.Parse()
 	start := time.Now()
-	inputPath = "./input1.yaml"
-	outputPath = "./output1.yaml"
-	err := cmd.RecoverKeysCmd(inputPath, outputPath)
+	err := cmd.RecoverKeysCmd(*inputPath, *outputPath)
 	if err != nil {
 		common.Logger.Errorf("%s", err)
 	}
 	duration := time.Since(start)
-	fmt.Printf("Output the result to file `%s`, cost: %s \n ", outputPath, duration.String())
+	fmt.Printf("Output the result to file `%s`, cost: %s \n ", *outputPath, duration.String())
 }
