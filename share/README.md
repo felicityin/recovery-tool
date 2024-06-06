a023d273f4dbed509de1518eb5d3f085d1e171d9
bingli <bingli@legend.tech>
2024-06-06 17:27:37 +0800

    share lib

--- share/README.md
+++ share/README.md
@@ -9,7 +9,7 @@ GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -ldflags "-s -w" -buildmode=c-s
 
 GOOS=darwin GOARCH=amd64 CGO_ENABLED=1 go build -buildmode=c-shared -o lib_recovery_tool.dylib main.go
 
-GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -buildmode=c-shared -o lib_recovery_tool.dylib main.go
+GOOS=darwin GOARCH=arm64 CGO_ENABLED=1 go build -buildmode=c-shared -o lib_recovery_tool_arm64.dylib main.go
 
 CGO_ENABLED=1 GOARCH=arm64  GOOS=linux   CC=arm-linux-musleabihf-gcc CGO_LDFLAGS="-static" go build -a -v -installsuffix cgo -o bin/bfs-data-detection .
 
--- share/main_test.go
+++ share/main_test.go
@@ -1,31 +1,22 @@
 package main
 
-import (
-	"github.com/alecthomas/gometalinter/_linters/src/gopkg.in/yaml.v2"
-	"os"
-	"recovery-tool/cmd"
-	"strconv"
-	"strings"
-	"testing"
-)
-
 // must remove import "C"
-func TestGoRecoveryTest(t *testing.T) {
-	bytess, err := os.ReadFile("../input1.yaml")
-	if err != nil {
-		panic(err)
-	}
-
-	var input cmd.RecoveryInput
-	if err := yaml.UnmarshalStrict(bytess, &input); err != nil {
-		panic(err)
-	}
-
-	vaultCountStr := strconv.Itoa(input.VaultCount)
-	chainStr := ""
-	for _, chainName := range input.Chains {
-		chainStr += chainName + ","
-	}
-	chainStr = strings.TrimRight(chainStr, ",")
-	GoRecoveryTest(input.ZipPath, input.UserMnemonic, input.EciesPrivKey, "./test/RSAKet", vaultCountStr, chainStr, "zh")
-}
+//func TestGoRecoveryTest(t *testing.T) {
+//	bytess, err := os.ReadFile("../input1.yaml")
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	var input cmd.RecoveryInput
+//	if err := yaml.UnmarshalStrict(bytess, &input); err != nil {
+//		panic(err)
+//	}
+//
+//	vaultCountStr := strconv.Itoa(input.VaultCount)
+//	chainStr := ""
+//	for _, chainName := range input.Chains {
+//		chainStr += chainName + ","
+//	}
+//	chainStr = strings.TrimRight(chainStr, ",")
+//	GoRecoveryTest(input.ZipPath, input.UserMnemonic, input.EciesPrivKey, "./test/RSAKet", vaultCountStr, chainStr, "zh")
+//}
